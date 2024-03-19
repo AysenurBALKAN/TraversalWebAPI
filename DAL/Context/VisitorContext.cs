db8a8fd71f5264910d7b63ad2b16b926a3c05a77
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-19 12:47:39 +0300

    Add project files.

---
+++ DAL/Context/VisitorContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalWebAPI.DAL.Entities;
+
+namespace TraversalWebAPI.DAL.Context
+{
+    public class VisitorContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=DESKTOP-HN2G937\\SQLSERVER;Initial Catalog=CoreTraversalAPI;user id=sa; password=153624;integrated security=false;");
+           
+    }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
