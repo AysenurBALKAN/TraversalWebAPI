f65f480a49680bfa6aaccc2d235521edfe1bdf69
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-19 15:20:34 +0300

    HTTP Attributes Operations

--- Controllers/VisitorController.cs
+++ Controllers/VisitorController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TraversalWebAPI.DAL.Context;
+using TraversalWebAPI.DAL.Entities;
 
 namespace TraversalWebAPI.Controllers
 {
@@ -11,5 +13,86 @@ namespace TraversalWebAPI.Controllers
     [ApiController]
     public class VisitorController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult VisitorList()
+        {
+            using(var c=new VisitorContext())
+            {
+                var values = c.Visitors.ToList();
+                return Ok(values);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddVisitor( Visitor visitor)
+        {
+            using (var c = new VisitorContext())
+            {
+                c.Add(visitor);
+                c.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult VisitorById(int id)
+        {
+            using (var c = new VisitorContext())
+            {
+                var values = c.Visitors.Find(id);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(values);
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteVisitor(int id)
+        {
+            using (var c = new VisitorContext())
+            {
+                var values = c.Visitors.Find(id);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    c.Remove(values);
+                    c.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateVisitor(Visitor visitor)
+        {
+            using (var c = new VisitorContext())
+            {
+                var values = c.Find<Visitor>(visitor.VisitorId);
+                if (values == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    values.City = visitor.City;
+                    values.Name = visitor.Name;
+                    values.SurName = visitor.SurName;
+                    values.Mail = visitor.Mail;
+                    values.Country = visitor.Country;
+                    c.Update(values);
+                    c.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
     }
 }
